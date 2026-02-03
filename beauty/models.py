a286fed297b8a8baedf933831598727a5436697e
likithac55 <likithachenna55@gmail.com>
2026-02-03 17:45:23 +0530

    Initial commit – Beauty Parlour Management System

---
+++ beauty/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class User_status(models.Model):
+    status = models.CharField(max_length=100,null=True)
+    def __str__(self):
+        return self.status
+
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    status = models.ForeignKey(User_status,on_delete=models.CASCADE,null=True)
+    mobile = models.IntegerField(null=True)
+    # id_card_no = models.CharField(max_length=100,null=True)
+    gender = models.CharField(max_length=10,null=True)
+    image = models.ImageField(null=True,blank=True)
+    def __str__(self):
+        return self.user.username
+
+class Servise(models.Model):
+    name = models.CharField(max_length=100,null=True)
+    cost = models.IntegerField(null=True)
+    image = models.FileField(null=True)
+    def __str__(self):
+        return self.name
+
+class Book_status(models.Model):
+    status = models.CharField(max_length=100,null=True)
+    def __str__(self):
+        return self.status
+
+class Booking_Paid(models.Model):
+    paid = models.CharField(max_length=100,null=True)
+    def __str__(self):
+        return self.paid
+
+class Apponitment(models.Model):
+    service = models.ForeignKey(Servise,on_delete=models.CASCADE,null=True)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE,null=True)
+    status = models.ForeignKey(Book_status,on_delete=models.CASCADE,null=True)
+    paid = models.ForeignKey(Booking_Paid,on_delete=models.CASCADE,null=True)
+    date1 = models.DateField(null=True)
+    time1 = models.TimeField(null=True)
+    def __str__(self):
+        return self.customer.user.username+" "+self.service.name
+
+# models.py
+
+class PaymentCard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link the payment card to a user
+    card_number = models.CharField(max_length=12, unique=True)  # Ensure the card number is unique
+
+    def __str__(self):
+        return f"{self.user.username} - {self.card_number}"
+
